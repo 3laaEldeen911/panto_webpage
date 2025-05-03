0293c4f96881d79e159b70a7a7fbaec7d8dd7cb1
3laaEldeen911 <324240231@sha.edu.eg>
2025-05-04 01:07:00 +0300

    finsh

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+document.querySelectorAll('.add-btn').forEach(function(button) {
+    button.addEventListener('click', function() {
+        alert('Item has been added to your cart!');
+    });
+});
